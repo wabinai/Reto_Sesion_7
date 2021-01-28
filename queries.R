86d2eceaa396fcb90d842f98df4287d831068519
wabinai <911nailarw@gmail.com>
2021-01-28 02:33:13 +0000

    Reto 1

---
+++ queries.R
@@ -0,0 +1,9 @@
+library(DBI)
+library(RMySQL)
+
+MyDataBase <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest")
