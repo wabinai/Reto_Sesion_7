c01f13d695f9fc9639b96a808b13b870e33b9cbb
wabinai <911nailarw@gmail.com>
2021-01-28 02:37:00 +0000

    reto1

--- queries.R
+++ queries.R
@@ -7,3 +7,14 @@ MyDataBase <- dbConnect(
   host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
   username = "guest",
   password = "guest")
+
+dbListTables(MyDataBase)
+dbListFields(MyDataBase, 'CountryLanguage')
+data1 <- dbGetQuery(MyDataBase, "select * from CountryLanguage")
+head(data1)
+
+library(dplyr)
+español<-filter(data1, Language == "Spanish") #funcion de dplyr
+
+library(ggplot2)
+ggplot(español,aes(CountryCode,Percentage))+geom_bar(stat = "identity", aes(fill=IsOfficial)) + coord_flip()
